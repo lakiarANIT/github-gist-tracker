ef7db0cc5436d8b2400826505e13d120eb033798
langat <kipngenolangat36@gmail.com>
2025-03-19 16:04:30 +0300

    testd tailwind css work perfectly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 >To add navbar</h1>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">Tailwind CSS Works!</h1>
+      </div>
+      <h1 className="text-color-blue size-1.2">To add navbar</h1>
       <h1>To add body</h1>
       <h1>To add Footer</h1>
     </div>
